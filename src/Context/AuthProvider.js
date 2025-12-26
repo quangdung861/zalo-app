8ed4847cb9c961c2dc575cc62efd089eb5b6b44a
quangdung861 <quangdung861@gmail.com>
2025-12-26 18:49:29 +0700

    Update: when redirected to an incorrect URL, it will automatically redirect to the Login page.

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -39,7 +39,7 @@ const AuthProvider = ({ children }) => {
         return navigate("/");
       }
       setIsLoading(false);
-      // navigate("/login");
+      navigate("/login");
     });
 
     return () => {
